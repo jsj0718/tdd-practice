e89d1d08d1b08d827af1fd51311f915a040e52d7
sjjeong <sjjeong-at-570146075219>
2022-06-07 17:42:45 +0900

    1. 화폐 예제

---
+++ src/main/java/me/jsj/money/Dollar.java
@@ -0,0 +1,15 @@
+package me.jsj.money;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        this.amount *= multiplier;
+    }
+
+}
