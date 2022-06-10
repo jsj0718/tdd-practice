959a077593fc5baad01595e4600ba6415b47eddb
sjjeong <sjjeong-at-570146075219>
2022-06-10 14:23:37 +0900

    화폐 예제 마무리

---
+++ src/main/java/me/jsj/money/Expression.java
@@ -0,0 +1,10 @@
+package me.jsj.money;
+
+public interface Expression {
+    Money reduce(Bank bank, String to);
+
+    Expression plus(Expression addend);
+
+    Expression times(int multiplier);
+
+}
